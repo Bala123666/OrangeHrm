90b4df6d903bd4fbdf74466db77dd7eb5c430cab
Lenovo <Lenovo@DESKTOP-5RBIGOC>
2022-11-09 18:44:37 +0530

    testcase is passed

--- DemoProject/src/sampleprogrames/Test2.java
+++ DemoProject/src/sampleprogrames/Test2.java
@@ -2,9 +2,17 @@ package sampleprogrames;
 
 public class Test2 {
 
+	
 	public static void main(String[] args) {
 		System.out.println("This is Second Test ");
+
+for(int i=0;i<10;i++)
+{
+	System.out.println("This is second case is passed");
+	
+}
 
 	}
 
 }
+
